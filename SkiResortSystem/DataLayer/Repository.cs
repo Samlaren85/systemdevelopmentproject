a53b8699fb259dbabc7f3d5110583a36254fb730
StudentKeranen <s2108237@student.hb.se>
2023-09-15 11:54:55 +0200

    Satt upp DataLayer

---
+++ SkiResortSystem/DataLayer/Repository.cs
@@ -0,0 +1,112 @@
+﻿using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DataLayer
+{
+    /// <summary>
+    ///  Generic repository class.
+    /// </summary>
+    public class Repository<T>
+        where T : class
+    {
+
+        protected AppDbContext Context { get; }
+        protected DbSet<T> Table { get; }
+
+        internal Repository(AppDbContext context)
+        {
+            Context = context;
+            Table = Context.Set<T>();
+        }
+
+        /// <summary>
+        ///  Add a new entity to the Table.
+        /// </summary>
+        /// <param name="entity"></param>
+        public void Add(T entity)
+        {
+            Table.Add(entity);
+        }
+
+        /// <summary>
+        /// Updatein entity with new attributes
+        /// </summary>
+        /// <param name="entity"></param>
+        public void Update(T entity)
+        {
+            T updEntity = Table.Find(entity);
+            if (updEntity != null)
+            {
+                Context.Entry(updEntity).CurrentValues.SetValues(entity);
+            }
+        }
+
+        /// <summary>
+        ///  Remove an entity from the Table.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns>true if removed and false otherwise.</returns>
+        public bool Remove(T entity)
+        {
+            Table.Remove(entity);
+            return Context.Entry(entity).State.Equals(EntityState.Deleted);
+        }
+
+        /// <summary>
+        ///  Find a set of entities that match a predicate.
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public IList<T> Find(Func<T, bool> predicate)
+        {
+            return Table.Where(predicate).ToList();
+        }
+
+        public IList<T> Find(Func<T, bool> predicate, params Expression<Func<T, object>>[] includes)
+        {
+            var query = Table.AsQueryable();
+            foreach (var include in includes)
+                query = query.Include(include);
+            return query.Where(predicate).ToList();
+        }
+
+        /// <summary>
+        ///  Find the first entity that match a predicate.
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public T FirstOrDefault(Func<T, bool> predicate)
+        {
+            return Table.FirstOrDefault(predicate);
+        }
+
+        public T FirstOrDefault(Func<T, bool> predicate, params Expression<Func<T, object>>[] includes)
+        {
+            var query = Table.AsQueryable();
+            foreach (var include in includes)
+                query = query.Include(include);
+            return query.FirstOrDefault(predicate);
+        }
+
+        /// <summary>
+        ///  Is this repository empty?
+        /// </summary>
+        /// <returns>true is it is empty, false otherwise.</returns>
+        public bool IsEmpty()
+        {
+            return Table.Count() == 0;
+        }
+
+        /// <summary>
+        ///  Count the entities in the Table.
+        /// </summary>
+        /// <returns>the number of entities.</returns>
+        public int Count()
+        {
+            return Table.Count();
+        }
+    }
+}
---
+++ SkiResortSystem/DataLayer/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class UnitOfWork
+    {
+        protected AppDbContext appDbContext { get; }
+
+        public UnitOfWork()
+        {
+            appDbContext = new AppDbContext();
+
+        }
+        public void Save()
+        {
+            appDbContext.SaveChanges();
+        }
+    }
+}

1863c056a063ab32d55ae8a9c89e6a0b4cbb5b50
StudentKeranen <s2108237@student.hb.se>
2023-09-15 13:52:54 +0200

    Lägger till Delete metod i UnitOfWork

--- SkiResortSystem/DataLayer/UnitOfWork.cs
+++ SkiResortSystem/DataLayer/UnitOfWork.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Entitylayer;
 
 namespace DataLayer
 {
     public class UnitOfWork
     {
         protected AppDbContext appDbContext { get; }
-        public Repository<User> UserRepository { get; private set; }
 
         public UnitOfWork()
         {
             appDbContext = new AppDbContext();
 
-            UserRepository = new Repository<User>(appDbContext);
+        }
 
+        public void Delete()
+        {
+            //Not implemented!
         }
         public void Save()
         {

3b13623788b242d96af721f92498adfbbfdee288
Jonathan Öberg <s2105325@student.hb.se>
2023-09-15 13:51:20 +0200

    UnitOfWork uppdaterat med UserRepository

--- SkiResortSystem/DataLayer/UnitOfWork.cs
+++ SkiResortSystem/DataLayer/UnitOfWork.cs
@@ -10,7 +10,7 @@ namespace DataLayer
     public class UnitOfWork
     {
         protected AppDbContext appDbContext { get; }
-        public Repository<User> UserRepository { get; private set; }
+        public Repository<User> UserRepository { get; private set; } //nu finns du med
 
         public UnitOfWork()
         {

a5ddf92ecbc7611e7f48fc8ec831ad6730f9e0be
StudentKeranen <s2108237@student.hb.se>
2023-09-23 09:41:52 +0200

    Fixade kontakten med servern. Behövde köra Encrypt=False

--- SkiResortSystem/DataLayer/AppDbContext.cs
+++ SkiResortSystem/DataLayer/AppDbContext.cs
@@ -8,12 +8,27 @@ namespace DataLayer
     {
         public DbSet<User> Users { get; set; } = null!;
 
+        public AppDbContext()
+        {
+            base.Database.EnsureCreated();
+            Seed();
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
         {
             if (!optionBuilder.IsConfigured)
             {
-                optionBuilder.UseSqlServer("Server=sqlutb2.hb.se,56077;Database=suht2301;User Id=suht2301;Password=bax999;");
+                optionBuilder.UseSqlServer("Server=sqlutb2.hb.se,56077;Database=suht2301;User Id=suht2301;Password=bax999;Encrypt=False");
                 base.OnConfiguring(optionBuilder);
+            }
+        }
+
+        private void Seed()
+        {
+            if (!Users.Any()) 
+            {
+                Users.Add(new User("Admin", 1));
+                Users.Add(new User("Password", 2));
+                SaveChanges();
             }
         }
 

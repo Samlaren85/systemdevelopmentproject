f76207e983b6d9c51abbfb1a742017e4c8d260e6
Danny Mansour <s205593@student.hb.se>
2023-09-29 12:03:56 +0200

    Lagt till kundeöversikt (privat, företag)

--- SkiResortSystem/SkiResortSystem/Components/BoendeModulen.xaml.cs
+++ SkiResortSystem/SkiResortSystem/Components/BoendeModulen.xaml.cs
@@ -24,5 +24,10 @@ namespace SkiResortSystem.Components
         {
             InitializeComponent();
         }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }

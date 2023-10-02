010b759a0b3dc3535f3b15d525b02df785ebd9e4
Danny Mansour <s205593@student.hb.se>
2023-10-02 13:38:03 +0200

    ändrat i kundöverikten

--- SkiResortSystem/SkiResortSystem/Components/KundModulen.xaml.cs
+++ SkiResortSystem/SkiResortSystem/Components/KundModulen.xaml.cs
@@ -25,5 +25,9 @@ namespace SkiResortSystem.Components
             InitializeComponent();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }

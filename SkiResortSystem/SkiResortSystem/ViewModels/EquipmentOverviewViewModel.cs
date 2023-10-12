50bf5fe9e1abdb235a6efebd37befd9dd47cac03
Danny Mansour <s205593@student.hb.se>
2023-10-12 15:33:19 +0200

    Aktivitet bodende ekonomi modulen, ändrade

--- SkiResortSystem/SkiResortSystem/ViewModels/EquipmentOverviewViewModel.cs
+++ SkiResortSystem/SkiResortSystem/ViewModels/EquipmentOverviewViewModel.cs
@@ -1,17 +1,18 @@
-﻿using SkiResortSystem.Models;
+﻿using SkiResortSystem.Commands;
+using SkiResortSystem.Models;
+using SkiResortSystem.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SkiResortSystem.ViewModels
 {
-    class EquipmentOverviewViewModel: ObservableObject
+    public class EquipmentOverviewViewModel : ObservableObject
     {
-        public EquipmentOverviewViewModel()
-        {
 
-        }
+       
     }
 }

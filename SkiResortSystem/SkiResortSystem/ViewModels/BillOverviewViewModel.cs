ec6e6126ea18363f201fa942cb040a2e7c778a8e
Danny Mansour <s205593@student.hb.se>
2023-10-16 10:09:53 +0200

    lagt till closecommand i billoverview

--- SkiResortSystem/SkiResortSystem/ViewModels/BillOverviewViewModel.cs
+++ SkiResortSystem/SkiResortSystem/ViewModels/BillOverviewViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using SkiResortSystem.Commands;
+using SkiResortSystem.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SkiResortSystem.ViewModels
 {
     class BillOverviewViewModel
     {
+        private ICommand closeCommand = null!;
+        public ICommand CloseCommand =>
+            closeCommand ??= closeCommand = new RelayCommand<ICloseable>((closeable) =>
+            {
+                closeable.Close();
+            });
     }
 }

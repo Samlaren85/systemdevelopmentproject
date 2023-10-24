84a163b22cc4b277734b1c7ba6d7213a4d21f113
Danny Mansour <s205593@student.hb.se>
2023-10-24 15:16:32 +0200

    lagt till boderbrush null ändrat visibility för olika användaren

--- SkiResortSystem/SkiResortSystem/ViewModels/MainViewModel.cs
+++ SkiResortSystem/SkiResortSystem/ViewModels/MainViewModel.cs
@@ -279,7 +279,7 @@ namespace SkiResortSystem.ViewModels
                         //UseShopOrReception = true;
                         Economy = Visibility.Visible;
                         MarketingManager = Visibility.Collapsed;
-                        Customer = Visibility.Visible;
+                        Customer = Visibility.Collapsed;
                         Governance = Visibility.Collapsed;
                         CEO = Visibility.Collapsed;
                         Shop = Visibility.Collapsed;
@@ -303,7 +303,7 @@ namespace SkiResortSystem.ViewModels
                         //UseShopOrReception = true;
                         Economy = Visibility.Visible;
                         MarketingManager = Visibility.Collapsed;
-                        Customer = Visibility.Visible;
+                        Customer = Visibility.Collapsed;
                         Governance = Visibility.Visible;
                         CEO = Visibility.Collapsed;
                         Reception = Visibility.Collapsed;
@@ -327,7 +327,7 @@ namespace SkiResortSystem.ViewModels
                         //UseShopOrReception = true;
                         Economy = Visibility.Collapsed;
                         MarketingManager = Visibility.Collapsed;
-                        Customer = Visibility.Visible;
+                        Customer = Visibility.Collapsed;
                         Governance = Visibility.Visible;
                         CEO = Visibility.Visible;
                         Reception = Visibility.Collapsed;
